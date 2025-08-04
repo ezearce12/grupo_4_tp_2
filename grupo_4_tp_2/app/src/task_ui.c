ab3214d1e4bd921ff591d86d4e9f601967353ba3
Fernando Folmer <ferfolmer95@gmail.com>
2025-08-04 18:24:44 -0300

    borrado de task+queue de los AOs

--- grupo_4_tp_2/app/src/task_ui.c
+++ grupo_4_tp_2/app/src/task_ui.c
@@ -93,22 +93,22 @@ static void send_led(ao_led_handle_t *h, ao_led_action_t a) {
   ao_led_dispatch(h, m);
 }
 
-void task_reactor(void *arg) {
+static void task_reactor(void *arg) {
   ui_message_t *uim;
   static msg_event_t last = MSG_EVENT_NONE;
+  static bool leds_init = false;
 
-  LOGGER_INFO("last value %d", last);
-
-  /* Apagar todo al arrancar */
-  send_led(&led_red, AO_LED_MESSAGE_OFF);
-  send_led(&led_green, AO_LED_MESSAGE_OFF);
-  send_led(&led_blue, AO_LED_MESSAGE_OFF);
+  if (!leds_init) {
+    send_led(&led_red, AO_LED_MESSAGE_OFF);
+    send_led(&led_green, AO_LED_MESSAGE_OFF);
+    send_led(&led_blue, AO_LED_MESSAGE_OFF);
+    leds_init = true;
+  }
 
   for (;;) {
     LOGGER_INFO("Reactor: waiting UI msg…");
     if (xQueueReceive(q_reactor, &uim, portMAX_DELAY) != pdPASS)
       continue;
-    LOGGER_INFO("last value %d", last);
 
     LOGGER_INFO("Reactor: got UI event=%d  last=%d", uim->event, last);
 
@@ -137,42 +137,47 @@ void task_reactor(void *arg) {
     if (uim->callback)
       uim->callback(uim->context);
     vPortFree(uim);
+
+    if (0 == uxQueueMessagesWaiting(q_reactor)) {
+      vQueueDelete(q_reactor);
+      q_reactor = NULL;
+      LOGGER_INFO("Reactor idle: resources freed");
+      vTaskDelete(NULL);
+      return;
+    }
   }
 }
 
 bool ao_ui_send_event(ui_message_t *msg) {
-  return xQueueSend(q_reactor, &msg, portMAX_DELAY) == pdPASS;
+
+  if (NULL == q_reactor) {
+    q_reactor = xQueueCreate(QUEUE_LENGTH_, QUEUE_ITEM_SIZE_);
+    while (NULL == q_reactor) {
+      // error
+    }
+
+    BaseType_t status;
+    status = xTaskCreate(task_reactor, "task_reactor", 256, NULL,
+                         tskIDLE_PRIORITY + 1, NULL);
+    while (pdPASS != status) {
+      // error
+    }
+  }
+  LOGGER_INFO("AO UI - Creating AO dinamically");
+  return pdPASS == xQueueSend(q_reactor, &msg, portMAX_DELAY);
 }
 
-void ao_ui_init(void) {
-  q_reactor = xQueueCreate(QUEUE_LENGTH_, QUEUE_ITEM_SIZE_);
-  while (NULL == q_reactor) {
-    // error
-  }
-
-  BaseType_t status;
-  status = xTaskCreate(task_reactor, "task_reactor", 256, NULL,
-                       tskIDLE_PRIORITY + 1, NULL);
-  while (pdPASS != status) {
-    // error
-  }
-}
-
-// void ao_ui_turOffLeds(void){
-//     for (int i = 0; i < 3; ++i) {
-//         ao_led_message_t* init_msg = pvPortMalloc(sizeof(ao_led_message_t));
-//         if (init_msg) {
-//             init_msg->id = 0;
-//             init_msg->callback = led_cb;
-//             init_msg->action = AO_LED_MESSAGE_OFF;
-//             init_msg->value = 0;
+// void ao_ui_init(void) {
+//   q_reactor = xQueueCreate(QUEUE_LENGTH_, QUEUE_ITEM_SIZE_);
+//   while (NULL == q_reactor) {
+//     // error
+//   }
 //
-//             switch (i) {
-//                 case 0: ao_led_send(&led_red, init_msg); break;
-//                 case 1: ao_led_send(&led_green, init_msg); break;
-//                 case 2: ao_led_send(&led_blue, init_msg); break;
-//             }
-//         }
-//     }
+//   BaseType_t status;
+//   status = xTaskCreate(task_reactor, "task_reactor", 256, NULL,
+//                        tskIDLE_PRIORITY + 1, NULL);
+//   while (pdPASS != status) {
+//     // error
+//   }
 // }
 /********************** end of file ******************************************/
